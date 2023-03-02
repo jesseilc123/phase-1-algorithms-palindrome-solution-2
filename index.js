7e9d160d6e4b3d79a7ac001ad6979a45b8364187
Jesse Ilc <jesseilc123@gmail.com>
2023-03-02 12:57:17 -0600

    Lab complete

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for(let i = 0; i < word.length / 2; i++){
+    const j = word.length - 1 - i
+    if(word[i] !== word[j]){ 
+      return false;
+    }
+  }
+  return true;
 }
+/* 
+make a function that returns true if a word is a palindrome. That means if the 
+first letter is the same as last letter, and the second letter is the same
+as the second to last letter, etc. until we reach the middle, return true.
+/* 
 
 /* 
   Add your pseudocode here
+
+  iterate from the beginning of the string to the middle 
+    check each letter to the correponding letter from the end
+      if any letters don't match, return false
+
+  if we reach the middle, and all the letters match, return true 
+
 */
 
 /*
@@ -20,6 +38,26 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
 }
 
 module.exports = isPalindrome;
